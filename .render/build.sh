0d2e16d22d4e92fd2ba0116c7fb6d8b3bdf41b40
Abumusa9 <phpstation9@gmail.com>
2025-07-31 15:48:31 +0100

    Configure render.yaml and build script

---
+++ .render/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit on errors
+set -e
+
+echo "Installing frontend dependencies..."
+cd ../sentiment_dashboard
+npm install
+
+echo "Building React app..."
+npm run build
+
+echo "Moving frontend build to backend static folder..."
+rm -rf ../sentiment_backend/src/static
+mv build ../sentiment_backend/src/static
+
+echo "Installing backend dependencies..."
+cd ../sentiment_backend
+pip install -r requirements.txt
